d11284cad31f8a36785069fa87b249e87187e167
madelinestaples <madeline.staples@umontana.edu>
2022-03-14 12:53:13 -0600

    3/14

--- Week 7 /hw7.js
+++ Week 7 /hw7.js
@@ -4,6 +4,10 @@
 var dog;
 var dog2;
 var blossum;
+//particle variable
+var particles = []
+
+
 function preload() {
  //dog = loadAnimation('Idle1.png', 'Idle10.png');
  //dog2 = loadAnimation('Walk1.png', 'Walk10.png');
@@ -17,16 +21,36 @@ function setup() {
     blossum = createSprite(700,100);
     blossum.addImage(loadImage('blossum.jpeg'));
   }
-  
-  function draw() {
+  function createParticles(x,y)
+  {
+  for (let i = 0; i < 5; i++) {
+      let p = new Particle(x,y);
+      particles.push(p);
+    }
+    for (let i = particles.length - 1; i >= 0; i--) {
+      particles[i].update();
+      particles[i].show();
+      if (particles[i].finished()) {
+        // remove this particle
+        particles.splice(i, 1);
+      }
+    }
+  }
+
+  function draw(){
   background(0);
   //animation(dog,300,150);
   if(keyDown('d'))
   {
     dog.changeAnimation('Walk');
     dog.velocity.x += .5;
-    if(dog.collide(blossum))
-    dog.changeAnimation('idle');
+    if(dog.collide(blossum)){
+      blossum.remove();
+      createParticles(blossum.position.x, blossum.position.y)
+      createYouWin()
+      dog.changeAnimation('idle');
+    }
+    
   }
 
   else if(keyDown('a')){
@@ -102,3 +126,10 @@ function createCharacter()
   circle(30,90,50);
   
   }
+  function createYouWin()
+{
+    textSize(32);
+    fill(255);
+    textSize(40);
+    text ('You Win',50, 300);
+}
